1fbab84df1e2d854720b32b33dfddd967d6be389
Mohammed Yasser <mohammedre4a@gmail.com>
2021-12-05 17:44:38 +0200

    Add: added variants of Splash activity for tablets and landscapes. refactor: change status bar color to secondaryVariant.

---
+++ app/src/main/java/com/myasser/eisenhowertodo/Splash.kt
@@ -0,0 +1,17 @@
+package com.myasser.eisenhowertodo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import java.util.*
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        val intent = Intent(this, ToDoList::class.java)
+        Handler(Looper.getMainLooper()).postDelayed({ startActivity(intent) }, 2000)
+    }
+}
