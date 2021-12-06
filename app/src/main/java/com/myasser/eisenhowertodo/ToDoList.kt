8d58963bf1872311367b6d579f6430927e4502ad
Mohammed Yasser <mohammedre4a@gmail.com>
2021-12-06 10:06:50 +0200

    Add: added onClickListener for the navigation button

--- app/src/main/java/com/myasser/eisenhowertodo/ToDoList.kt
+++ app/src/main/java/com/myasser/eisenhowertodo/ToDoList.kt
@@ -1,11 +1,13 @@
 package com.myasser.eisenhowertodo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.inputmethod.EditorInfo
 import com.myasser.eisenhowertodo.databinding.ActivityToDoListBinding
 
-class ToDoList : AppCompatActivity() {
+class ToDoList : AppCompatActivity(), View.OnClickListener {
     lateinit var binding: ActivityToDoListBinding
     private val tasks = arrayListOf<Task>()
     private var taskListString = ""
@@ -14,6 +16,7 @@ class ToDoList : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityToDoListBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.navigateButton.setOnClickListener(this)
         binding.taskInputLayout.imeOptions = EditorInfo.IME_ACTION_NEXT
         binding.taskInputLayout.setOnEditorActionListener { _, i, _ ->
             if (i == EditorInfo.IME_ACTION_NEXT) {
@@ -24,7 +27,7 @@ class ToDoList : AppCompatActivity() {
         }
     }
 
-    private fun getTasks(): List<Task> {
+    fun getTasks(): List<Task> {
         return tasks
     }
 
@@ -34,5 +37,13 @@ class ToDoList : AppCompatActivity() {
         taskListString += "${textCounter++}: ${binding.taskInputLayout.text.toString()}\n"
         binding.taskList.text = taskListString
         binding.taskInputLayout.text?.clear()
+    }
+
+    override fun onClick(p0: View?) {
+        if (p0?.id == R.id.navigateButton) {
+            val intent: Intent = Intent(this, EisenhowerList::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
