2029c0090e2087663d4ba40613c94a39bde4679d
Mohammed Yasser <mohammedre4a@gmail.com>
2021-12-10 16:52:28 +0200

    Add: added style for check box and sound of ticking. refactor: change background of fragment radius.

--- app/src/main/java/com/myasser/eisenhowertodo/DoTask.kt
+++ app/src/main/java/com/myasser/eisenhowertodo/DoTask.kt
@@ -1,18 +1,20 @@
 package com.myasser.eisenhowertodo
 
 import android.graphics.Paint
+import android.media.MediaPlayer
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Layout
+import android.os.Handler
+import android.os.Looper
 import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
 import android.widget.LinearLayout
 import androidx.annotation.RequiresApi
+import com.google.android.material.resources.TextAppearance
 import com.myasser.eisenhowertodo.databinding.ActivityDoTaskBinding
-import org.w3c.dom.Text
 
 class DoTask : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
@@ -24,17 +26,12 @@ class DoTask : AppCompatActivity() {
         val databaseHelper = DatabaseHelper(applicationContext)
         val params = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
         for (task in databaseHelper.readData(Classified.Do)) {
-            val checkBox = CheckBox(this)
+            val checkBox = CheckBox(this, null, 0, R.style.checkBoxText)
             checkBox.text = task.getName()
-            checkBox.textSize = 25f
-            checkBox.gravity = Gravity.CENTER
-            checkBox.textAlignment = View.TEXT_ALIGNMENT_TEXT_START
-            checkBox.setTextColor(resources.getColor(R.color.white))
-            params.setMargins(5, 10, 5, 10)
-            checkBox.layoutParams = params
             checkBox.setOnCheckedChangeListener { _, _ ->
                 checkBox.visibility = ViewGroup.GONE
                 val databaseHelper = DatabaseHelper(applicationContext)
+                MediaPlayer.create(this,R.raw.check_sound).start()
                 databaseHelper.deleteTask(task)
             }
             bind.doNowListLayout.addView(checkBox)
